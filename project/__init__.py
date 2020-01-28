cfbb2e6abe03c3af378df829a1ef370de8cd24b4
zruxi <zruxi@vmware.com>
2020-01-28 15:38:16 -0800

    docker config

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,10 @@
 # project/__init__.py
 
-
+import os  # new
 from flask import Flask, jsonify
 from flask_restplus import Resource, Api
 
+import sys
 
 # instantiate the app
 app = Flask(__name__)
@@ -11,8 +12,10 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = app.config.from_object('project.config.DevelopmentConfig')  # new
+app.config.from_object(app_settings)      # new
 
+print(app.config, file=sys.stderr)
 
 class Ping(Resource):
     def get(self):
